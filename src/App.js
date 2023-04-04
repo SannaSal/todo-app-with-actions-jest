3bb5f871cdeac987a629607588e6c3c92af9b25a
Sanna Salminen <sanna.salminen@edu.turkuamk.fi>
2023-04-04 16:30:51 +0300

    Create a new todo item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css'
+import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [todo, setTodo] = useState("");
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>React Todo App</h1>
+      <div className="input-wrapper">
+        <input type="text" name="todo" placeholder="Create a new todo" />
+        <input
+          type="text"
+          name="todo"
+          value={todo}
+          placeholder="Create a new todo"
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        />
+        <button className='add-button'>Add</button>
+      </div>
     </div>
   );
 }
